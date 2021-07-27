a33bf2bc5429419cf2f412aa8a69c3ce3a2db763
CAlexZA <alex.vanzyl@enerdyne.co.za>
2021-07-27 11:37:38 +0200

    Project Working.

--- ElecDev_Graphics_Application/Source/mainApplication.cpp
+++ ElecDev_Graphics_Application/Source/mainApplication.cpp
@@ -8,68 +8,68 @@
 #include <imstb_textedit.h>
 #include <imstb_truetype.h>
 
-int main(void)
-{
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-
-    ImGui::CreateContext();
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /*================================================================================================*/
-        /*Render ImGUI here. */
-        /*================================================================================================*/
-
-        ImGui::NewFrame();
-        ImGui::Begin("ElecDev Graphics");
-
-        ImGui::ShowDemoWindow();
-
-        /*================================================================================================*/
-        /* Render OpenGL here. */
-        /*================================================================================================*/
-
-        //glClear(GL_COLOR_BUFFER_BIT);
-
-        //glBegin(GL_TRIANGLES);
-        //glVertex2f(-0.5f, -0.5f);
-        //glVertex2f(0.0f, 0.5f);
-        //glVertex2f(0.5f, -0.5f);
-        //glEnd();
-
-        //glBegin(GL_LINE_LOOP);
-        //glVertex2f(-0.99, 1.0f);
-        //glVertex2f(0.99f, 0.99f);
-        //glVertex2f(1.0f, -1.0f);
-        //glVertex2f(-1.0f, -1.0f);
-        //glEnd();
-
-        ///* Swap front and back buffers */
-        //glfwSwapBuffers(window);
-
-        ///* Poll for and process events */
-        //glfwPollEvents();
-
-        /*================================================================================================*/
-    }
-
-    glfwTerminate();
-    return 0;
-}
+//int main(void)
+//{
+//    GLFWwindow* window;
+//
+//    /* Initialize the library */
+//    if (!glfwInit())
+//        return -1;
+//
+//    /* Create a windowed mode window and its OpenGL context */
+//    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+//    if (!window)
+//    {
+//        glfwTerminate();
+//        return -1;
+//    }
+//
+//
+//    ImGui::CreateContext();
+//
+//    /* Make the window's context current */
+//    glfwMakeContextCurrent(window);
+//
+//    /* Loop until the user closes the window */
+//    while (!glfwWindowShouldClose(window))
+//    {
+//        /*================================================================================================*/
+//        /*Render ImGUI here. */
+//        /*================================================================================================*/
+//
+//        ImGui::NewFrame();
+//        ImGui::Begin("ElecDev Graphics");
+//
+//        ImGui::ShowDemoWindow();
+//
+//        /*================================================================================================*/
+//        /* Render OpenGL here. */
+//        /*================================================================================================*/
+//
+//        //glClear(GL_COLOR_BUFFER_BIT);
+//
+//        //glBegin(GL_TRIANGLES);
+//        //glVertex2f(-0.5f, -0.5f);
+//        //glVertex2f(0.0f, 0.5f);
+//        //glVertex2f(0.5f, -0.5f);
+//        //glEnd();
+//
+//        //glBegin(GL_LINE_LOOP);
+//        //glVertex2f(-0.99, 1.0f);
+//        //glVertex2f(0.99f, 0.99f);
+//        //glVertex2f(1.0f, -1.0f);
+//        //glVertex2f(-1.0f, -1.0f);
+//        //glEnd();
+//
+//        ///* Swap front and back buffers */
+//        //glfwSwapBuffers(window);
+//
+//        ///* Poll for and process events */
+//        //glfwPollEvents();
+//
+//        /*================================================================================================*/
+//    }
+//
+//    glfwTerminate();
+//    return 0;
+//}
