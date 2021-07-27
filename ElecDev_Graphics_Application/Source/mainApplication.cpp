31cab6cc6c51c403a1e32566028b0318934e37c0
CAlexZA <alex.vanzyl@enerdyne.co.za>
2021-07-27 18:31:08 +0200

    Update mainApplication.cpp

--- ElecDev_Graphics_Application/Source/mainApplication.cpp
+++ ElecDev_Graphics_Application/Source/mainApplication.cpp
@@ -1,6 +1,6 @@
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*=======================================================================================================================================*/
 /* Includes                                                                                                                              */
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*=======================================================================================================================================*/
 
 // General.
 #include <stdio.h>
@@ -16,9 +16,9 @@
 // Include GLFW (window) after OpenGL definition.
 #include <GLFW/glfw3.h>
 
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
-/* Functions                                                                                                                             */
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*=======================================================================================================================================*/
+/* Functions.                                                                                                                             */
+/*=======================================================================================================================================*/
 
 // GLFW error handler.
 static void glfw_error_callback(int error, const char* description)
@@ -29,87 +29,100 @@ static void glfw_error_callback(int error, const char* description)
 // This removes the console that keeps opening with the app.
 #pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
 
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*=======================================================================================================================================*/
 /* Main                                                                                                                                  */
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*=======================================================================================================================================*/
 
 int main(int, char**)
 {
-    // Setup window
+
+    // Setup window.
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
         return 1;
 
-    // Decide GL+GLSL versions
-#if defined(IMGUI_IMPL_OPENGL_ES2)
-    // GL ES 2.0 + GLSL 100
-    const char* glsl_version = "#version 100";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
-#elif defined(__APPLE__)
-    // GL 3.2 + GLSL 150
-    const char* glsl_version = "#version 150";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
-#else
-    // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
-    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
-#endif
+    /*-----------------------------------------------------------------------------------------------------------------------------------*/
+    // OpenGL Version select.
+    /*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+    #if defined(IMGUI_IMPL_OPENGL_ES2)
+        // GL ES 2.0 + GLSL 100.
+        const char* glsl_version = "#version 100";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+    #elif defined(__APPLE__)
+        // GL 3.2 + GLSL 150.
+        const char* glsl_version = "#version 150";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only.
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac.
+    #else
+        // GL 3.0 + GLSL 130.
+        const char* glsl_version = "#version 130";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+        //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only.
+        //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only.
+    #endif
+
+    /*-----------------------------------------------------------------------------------------------------------------------------------*/
 
     // Create window with graphics context
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Dear ImGui GLFW+OpenGL3 example", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ElecDev Graphics", NULL, NULL);
     if (window == NULL)
         return 1;
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
+    glfwSwapInterval(1); // Enable vsync.
 
-    // Initialize OpenGL loader
-#if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
-    bool err = gl3wInit() != 0;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
-    bool err = glewInit() != GLEW_OK;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
-    bool err = gladLoadGL() == 0;
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
-    bool err = gladLoadGL(glfwGetProcAddress) == 0; // glad2 recommend using the windowing library loader instead of the (optionally) bundled one.
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
-    bool err = false;
-    glbinding::Binding::initialize();
-#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
-    bool err = false;
-    glbinding::initialize([](const char* name) { return (glbinding::ProcAddress)glfwGetProcAddress(name); });
-#else
-    bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
-#endif
+    /*-----------------------------------------------------------------------------------------------------------------------------------*/
+    // Initialize OpenGL loader.
+    /*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+    #if defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+            bool err = gl3wInit() != 0;
+    #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+            bool err = glewInit() != GLEW_OK;
+    #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+            bool err = gladLoadGL() == 0;
+    #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+            bool err = gladLoadGL(glfwGetProcAddress) == 0; // glad2 recommend using the windowing library loader instead of the (optionally) bundled one.
+    #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+            bool err = false;
+            glbinding::Binding::initialize();
+    #elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+            bool err = false;
+            glbinding::initialize([](const char* name) { return (glbinding::ProcAddress)glfwGetProcAddress(name); });
+    #else
+            bool err = false; // If you use IMGUI_IMPL_OPENGL_LOADER_CUSTOM, your loader is likely to requires some form of initialization.
+    #endif
+
+    // OpenGL loader error handler.
     if (err)
     {
         fprintf(stderr, "Failed to initialize OpenGL loader!\n");
         return 1;
     }
 
-    // Setup Dear ImGui context
+    /*-----------------------------------------------------------------------------------------------------------------------------------*/
+    // ImGUI setup.
+    /*-----------------------------------------------------------------------------------------------------------------------------------*/
+
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls.
 
-    // Setup Dear ImGui style
+    // Setup Dear ImGui style.
     ImGui::StyleColorsDark();
-    //ImGui::StyleColorsClassic();
+    //ImGui::StyleColorsClassic();  // Set mode to light.
 
-    // Setup Platform/Renderer backends
+    // Setup Platform/Renderer backends.
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    // Load Fonts
+    // Load Fonts.
     // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
     // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
     // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
@@ -124,19 +137,21 @@ int main(int, char**)
     //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
     //IM_ASSERT(font != NULL);
 
-    // Our state
+    /*-----------------------------------------------------------------------------------------------------------------------------------*/
+
+    // Set states.
     bool show_demo_window = true;
     bool show_another_window = true;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*=======================================================================================================================================*/
 /* Loop                                                                                                                                  */
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*=======================================================================================================================================*/
 
     // Main loop
     while (!glfwWindowShouldClose(window))
     {
-        // Poll and handle events (inputs, window resize, etc.)
+        // Poll and handle events (inputs, window resize, etc.).
         // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
         // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application.
         // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application.
@@ -207,8 +222,9 @@ int main(int, char**)
     glfwTerminate();
 
     return 0;
+
 }
 
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*=======================================================================================================================================*/
 /* EOF                                                                                                                                  */
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+/*=======================================================================================================================================*/
