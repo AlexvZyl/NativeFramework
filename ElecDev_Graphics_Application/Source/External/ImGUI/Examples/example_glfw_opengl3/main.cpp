93c813bc619e8360bdc6cff69c31da4e69460fa4
CAlexZA <alex.vanzyl@enerdyne.co.za>
2021-07-27 18:17:26 +0200

    Added mainApplication.cpp

--- ElecDev_Graphics_Application/Source/External/ImGUI/Examples/example_glfw_opengl3/main.cpp
+++ ElecDev_Graphics_Application/Source/External/ImGUI/Examples/example_glfw_opengl3/main.cpp
@@ -1,37 +1,55 @@
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
-/* Includes                                                                                                                              */
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+// Dear ImGui: standalone example application for GLFW + OpenGL 3, using programmable pipeline
+// (GLFW is a cross-platform general purpose library for handling windows, inputs, OpenGL/Vulkan/Metal graphics context creation, etc.)
+// If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
+// Read online: https://github.com/ocornut/imgui/tree/master/docs
 
-// General.
-#include <stdio.h>
-
-// ImGUI (GUI software). 
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include <stdio.h>
 
-// GLAD (OpenGL loader).
-#include <glad/glad.h>
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+// About Desktop OpenGL function loaders:
+//  Modern desktop OpenGL doesn't have a standard portable header file to load OpenGL function pointers.
+//  Helper libraries are often used for this purpose! Here we are supporting a few common ones (gl3w, glew, glad).
+//  You may use another loader/header of your choice (glext, glLoadGen, etc.), or chose to manually implement your own.
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GL3W)
+#include <GL/gl3w.h>            // Initialize with gl3wInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLEW)
+#include <GL/glew.h>            // Initialize with glewInit()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD)
+#include <glad/glad.h>          // Initialize with gladLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLAD2)
+#include <glad/gl.h>            // Initialize with gladLoadGL(...) or gladLoaderLoadGL()
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING2)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/Binding.h>  // Initialize with glbinding::Binding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#elif defined(IMGUI_IMPL_OPENGL_LOADER_GLBINDING3)
+#define GLFW_INCLUDE_NONE       // GLFW including OpenGL headers causes ambiguity or multiple definition errors.
+#include <glbinding/glbinding.h>// Initialize with glbinding::initialize()
+#include <glbinding/gl/gl.h>
+using namespace gl;
+#else
+#include IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#endif
 
-// Include GLFW (window) after OpenGL definition.
+// Include glfw3.h after our OpenGL definitions
 #include <GLFW/glfw3.h>
 
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
-/* Functions                                                                                                                             */
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
+// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
+// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
+// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
 
-// GLFW error handler.
 static void glfw_error_callback(int error, const char* description)
 {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
-
-// This removes the console that keeps opening with the app.
-#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
-
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
-/* Main                                                                                                                                  */
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
 
 int main(int, char**)
 {
@@ -126,12 +144,8 @@ int main(int, char**)
 
     // Our state
     bool show_demo_window = true;
-    bool show_another_window = true;
+    bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
-/* Loop                                                                                                                                  */
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
 
     // Main loop
     while (!glfwWindowShouldClose(window))
@@ -197,18 +211,13 @@ int main(int, char**)
         glfwSwapBuffers(window);
     }
 
-    // Cleanup.
+    // Cleanup
     ImGui_ImplOpenGL3_Shutdown();
     ImGui_ImplGlfw_Shutdown();
     ImGui::DestroyContext();
 
-    // Close application.
     glfwDestroyWindow(window);
     glfwTerminate();
 
     return 0;
 }
-
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
-/* EOF                                                                                                                                  */
-/*---------------------------------------------------------------------------------------------------------------------------------------*/
