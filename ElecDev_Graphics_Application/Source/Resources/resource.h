aa7c62f233489549dd927e4e92f2c5a51e18b94b
CAlexZA <alex.vanzyl@enerdyne.co.za>
2021-09-13 08:22:10 +0200

    Adding .rc files.

--- ElecDev_Graphics_Application/Source/Resources/resource.h
+++ ElecDev_Graphics_Application/Source/Resources/resource.h
@@ -1,13 +1,22 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by resourceHandler.rc
+//
+#define IDB_PNG1                        101
+#define IDB_PNG2                        102
+#define IDB_PNG3                        103
+#define IDI_ICON1                       107
+#define IDR_ACCELERATOR1                108
+#define IDR_SHADER1                     110
+#define IDR_SHADER2                     111
+#define IDR_SHADER3                     112
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        113
+#define _APS_NEXT_COMMAND_VALUE         40003
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
