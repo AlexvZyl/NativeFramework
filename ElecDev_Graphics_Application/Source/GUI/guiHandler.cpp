012edde4d068e3dd78bd628bc410fdcec6e82fdc
Raynhardt Van Zyl <raynhardt.vanzyl@gmail.com>
2021-09-15 20:11:43 +0200

    Docking

--- ElecDev_Graphics_Application/Source/GUI/guiHandler.cpp
+++ ElecDev_Graphics_Application/Source/GUI/guiHandler.cpp
@@ -29,10 +29,8 @@ GUIHandler::GUIHandler(stateMachine* states, GraphicsHandler* graphicsHandler)
 
 void GUIHandler::renderGraphics()
 {
-	
 
-
-	ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_NoDockingInCentralNode | ImGuiDockNodeFlags_PassthruCentralNode;
+	ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode;
 
 	ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
 	ImGuiViewport* viewport = ImGui::GetMainViewport();
@@ -41,17 +39,41 @@ void GUIHandler::renderGraphics()
 	ImGui::SetNextWindowPos(ImVec2(work_pos.x, work_pos.y + 50));
 	ImGui::SetNextWindowSize(ImVec2(work_size.x, work_size.y - 50));
 	ImGui::SetNextWindowViewport(viewport->ID);
-	window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
-	window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus | ImGuiWindowFlags_NoBackground;
 	bool p_open = true;
 	ImGui::Begin("DockSpace Demo", &p_open, window_flags);
+	ImGui::PopStyleVar(2);
+	ImGuiID dockspaceID = ImGui::GetID("MyDockSpace");
 
-	ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
-	ImGui::DockSpace(dockspace_id, ImVec2(0, 0), dockspace_flags);
+
+	
+	ImGui::DockBuilderAddNode(dockspaceID, ImGuiDockNodeFlags_None);
+
+	ImGuiID dock_main_id = dockspaceID;
+	ImGuiID dock_up_id = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Up, 0.05f, nullptr, &dock_main_id);
+	ImGuiID dock_right_id = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Right, 0.2f, nullptr, &dock_main_id);
+	ImGuiID dock_left_id = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Left, 0.2f, nullptr, &dock_main_id);
+	ImGuiID dock_down_id = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Down, 0.2f, nullptr, &dock_main_id);
+	ImGuiID dock_down_right_id = ImGui::DockBuilderSplitNode(dock_down_id, ImGuiDir_Right, 0.6f, nullptr, &dock_down_id);
+
+	ImGui::DockBuilderDockWindow("Actions", dock_up_id);
+	ImGui::DockBuilderDockWindow("Hierarchy", dock_right_id);
+	ImGui::DockBuilderDockWindow("Inspector", dock_left_id);
+	ImGui::DockBuilderDockWindow("Console", dock_down_id);
+	ImGui::DockBuilderDockWindow("Project", dock_down_right_id);
+	ImGui::DockBuilderDockWindow("Scene", dock_main_id);
+
+	ImGui::DockBuilderFinish(dock_main_id);
+	
+
+	ImGui::DockSpace(dockspaceID, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
 
 	this->toolbar->renderToolbar();
 
-	ImGui::SetNextWindowDockID(0, ImGuiCond_Once);
+	//ImGui::SetNextWindowDockID(0, ImGuiCond_Once);
 
 	this->ribbons->renderRibbons();
 
@@ -64,6 +86,9 @@ void GUIHandler::renderGraphics()
 	ImGui::End();	
 
 	ImGui::End();
+
+	ImGui::PopStyleVar();
+
 };
 
 void GUIHandler::setTheme() 
@@ -72,7 +97,6 @@ void GUIHandler::setTheme()
     ImGui::GetStyle().FrameRounding = 4.0f;
     ImGui::GetStyle().GrabRounding = 4.0f;
 	ImGui::GetStyle().ChildRounding = 4.0f;
-	//ImGui::GetStyle().WindowRounding = 4.0f;
 }
 //----------------------------------------------------------------------------------------------------------------------
 //  EOF.
