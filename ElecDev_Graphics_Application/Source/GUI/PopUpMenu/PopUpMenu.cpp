96c8a9e3f2e95d148d40ff18f4bb6bfb464174bf
AlexEnerdyne <81622310+AlexEnerdyne@users.noreply.github.com>
2021-11-14 17:55:20 +0200

    Input handling
    Input handling is now used on a flag basis.  The event is only implemented on each frame, not every time it is called.

--- ElecDev_Graphics_Application/Source/GUI/PopUpMenu/PopUpMenu.cpp
+++ ElecDev_Graphics_Application/Source/GUI/PopUpMenu/PopUpMenu.cpp
@@ -20,18 +20,24 @@ PopUpMenu::PopUpMenu(GUIState* guiState)
 // Render call.
 void PopUpMenu::render()
 {
+    // Place at mouse position.
     ImVec2 parentPos = ImGui::GetWindowPos();
     ImGui::SetNextWindowPos(ImVec2(m_guiState->popUpPosition.x + parentPos.x,
         m_guiState->popUpPosition.y + parentPos.y));
-    ImGui::Begin("PopUpMenu", &m_guiState->popUpMenu, ImGuiWindowFlags_NoDecoration |
-        ImGuiWindowFlags_NoMove |
-        ImGuiWindowFlags_NoDocking |
-        ImGuiWindowFlags_NoFocusOnAppearing);
 
-    if (ImGui::MenuItem("Add component", "C")) { /* Do stuff */ }
-    if (ImGui::MenuItem("Add port", "P")) { /* Do stuff */ }
-    ImGui::Separator();
-    if (ImGui::MenuItem("Remove component", "R")) { /* Do stuff */ }
+    // Popup menu.
+    //ImGui::Begin("PopUpMenu", &m_guiState->popUpMenu, ImGuiWindowFlags_NoDecoration |
+    //    ImGuiWindowFlags_NoMove |
+    //    ImGuiWindowFlags_NoDocking |
+    //    ImGuiWindowFlags_NoFocusOnAppearing);
+    //if (ImGui::MenuItem("Add component", "C")) { /* Do stuff */ }
+    //if (ImGui::MenuItem("Add port", "P")) { /* Do stuff */ }
+    //ImGui::Separator();
+    //if (ImGui::MenuItem("Remove component", "R")) { /* Do stuff */ }
+    //ImGui::End();
+
+    // Editor menu.
+    ImGui::Begin("Comoponent Editor");
     ImGui::End();
 }
 
