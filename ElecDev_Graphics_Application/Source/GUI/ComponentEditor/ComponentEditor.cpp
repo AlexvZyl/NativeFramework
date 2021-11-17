7b1bdd16c1916694fff8707a460b22d3436b53d6
Cullen Stewart-Burger <cullen.steward-burger@enerdyne.co.za>
2021-11-18 00:35:17 +0200

    Update ComponentEditor.cpp

--- ElecDev_Graphics_Application/Source/GUI/ComponentEditor/ComponentEditor.cpp
+++ ElecDev_Graphics_Application/Source/GUI/ComponentEditor/ComponentEditor.cpp
@@ -16,12 +16,32 @@ ComponentEditor::ComponentEditor(GUIState* guiState, GraphicsHandler* graphicsHa
 void ComponentEditor::render() 
 {
 	// Place editor at correct position.
-	ImGui::SetNextWindowPos(m_guiState->popUpPosition, ImGuiCond_Once);
+	ImGui::SetNextWindowPos(m_guiState->popUpPosition);
 	// Editor menu.
-	ImVec4 newCol = ImVec4(0.05f, 0.05, 0.07f, 0.9f);
+	ImVec4 newCol = ImVec4(0.05f, 0.05f, 0.07f, 0.9f);
 	ImGui::PushStyleColor(ImGuiCol_WindowBg, newCol);
 	ImGui::Begin("Comoponent Editor", &m_guiState->componentEditor, ImGuiWindowFlags_NoDocking);
-	ImGui::Button("New MCC");
+	//ImGui::Button("New MCC");
+	//ImGui::CollapsingHeader("Ports");
+	if (ImGui::TreeNode("Ports")) {
+		if (ImGui::TreeNodeEx("Left")) {
+
+			ImGui::TreePop();
+		}	
+		if (ImGui::TreeNode("Right")) {
+
+			ImGui::TreePop();
+		}
+		if (ImGui::TreeNode("Top")) {
+
+			ImGui::TreePop();
+		}
+		if (ImGui::TreeNode("Bottom")) {
+
+			ImGui::TreePop();
+		}
+		ImGui::TreePop();
+	}
 	ImGui::Separator();
 	ImGui::End();
 	ImGui::PopStyleColor();
