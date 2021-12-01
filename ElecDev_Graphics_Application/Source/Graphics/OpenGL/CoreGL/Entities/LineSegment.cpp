1ee4ac54bf68ccfe9f35a6e10bfb5f6d54d0274a
Cullen Stewart-Burger <cullen.steward-burger@enerdyne.co.za>
2021-12-01 17:56:42 +0200

    Added Boost (v1.17)

--- ElecDev_Graphics_Application/Source/Graphics/OpenGL/CoreGL/Entities/LineSegment.cpp
+++ ElecDev_Graphics_Application/Source/Graphics/OpenGL/CoreGL/Entities/LineSegment.cpp
@@ -1,6 +1,10 @@
 #include "LineSegment.h"
 
-LineSegment::LineSegment(VertexData start, VertexData end, float thickness, VertexArrayObject<VertexData>* VAO, ManagedEntity* parent)
+
+LineSegment::LineSegment(glm::vec2 start, glm::vec2 end, VertexArrayObject<VertexData>* VAO, ManagedEntity* parent, float thickness, glm::vec4 colour) 
+	:m_direction((end-start)/(glm::length(glm::vec2(end - start)))),
+	m_perpendicular(- m_direction::member.y, -m_direction::member.x)
+	Polygon2D<VertexData>(std::vector <glm::vec3>{glm::vec3(start-)})
 {
 
 }
