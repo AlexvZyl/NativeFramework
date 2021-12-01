798a4d12ef0a852d67f0962222ddea976dd03ca4
Cullen Stewart-Burger <cullen.steward-burger@enerdyne.co.za>
2021-12-01 16:21:25 +0200

    Update LineSegment.h

--- ElecDev_Graphics_Application/Source/Graphics/OpenGL/CoreGL/Entities/LineSegment.h
+++ ElecDev_Graphics_Application/Source/Graphics/OpenGL/CoreGL/Entities/LineSegment.h
@@ -1,16 +1,21 @@
 #pragma once
-#include "Entity.h"
 #include "Vertex.h"
+#include "Polygon.h"
 #include <memory>
+#include <boost/utility/base_from_member.hpp>
 class LineSegment :
-    public Entity<VertexData>
+    private boost::base_from_member< glm::vec2, 1>,
+    private boost::base_from_member< glm::vec2, 2>,
+    public Polygon2D<VertexData> {
+    using 
+};
 {
 private:
-
+    glm::vec2 m_direction;
+    glm::vec2 m_perpendicular;
 
 public:
-
-    LineSegment(VertexData start, VertexData end,  float thickness, VertexArrayObject<VertexData>* VAO, ManagedEntity* parent);
+    LineSegment(glm::vec2 start, glm::vec2 end, VertexArrayObject<VertexData>* VAO, ManagedEntity* parent, float thickness = 0.1f, glm::vec4 colour = { 0.f, 0.f, 0.f, 1.f });
 
 };
 
