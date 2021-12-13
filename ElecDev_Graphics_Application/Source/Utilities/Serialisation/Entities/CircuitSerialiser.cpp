fe3867360c2a88c5a848f294cb3c528942879b27
AlexEnerdyne <alex.vanzyl@enerdyne.co.za>
2021-12-13 09:36:18 +0200

    Component Editor Improvements

--- ElecDev_Graphics_Application/Source/Utilities/Serialisation/Entities/CircuitSerialiser.cpp
+++ ElecDev_Graphics_Application/Source/Utilities/Serialisation/Entities/CircuitSerialiser.cpp
@@ -191,12 +191,12 @@ void deserialise(YAML::Node& yamlNode, Design2DEngineGL& engine)
 		Port* endPort = dynamic_cast<Port*>(EntityManager::getEntity(idTable[cableNode["End port"].as<unsigned>()]));
 		// Create cable.
 		std::shared_ptr<Cable> cable = std::make_shared<Cable>(
-												startPort, 
-												nodeVector,
-												endPort,
-												engine.m_triangleEntitiesVAO.get(),
-												engine.m_circleEntitiesVAO.get(),
-												engine.m_circuit.get());
+													    startPort, 
+													    nodeVector,
+													    endPort,
+													    engine.m_triangleEntitiesVAO.get(),
+													    engine.m_circleEntitiesVAO.get(),
+													    engine.m_circuit.get());
 		cable->unhighlight();
 		// Add cable to circuit.
 		engine.m_circuit->m_cables.push_back(cable);
