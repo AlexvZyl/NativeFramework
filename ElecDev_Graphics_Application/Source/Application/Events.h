ff075700ef2e20ae2f3256efba839de167b1f885
AlexEnerdyne <81622310+AlexEnerdyne@users.noreply.github.com>
2022-01-15 15:29:00 +0200

    Events GLFW Implementation.

--- ElecDev_Graphics_Application/Source/Application/Events.h
+++ ElecDev_Graphics_Application/Source/Application/Events.h
@@ -6,9 +6,12 @@
 
 #include <stdint.h>
 #include "External/GLM/glm.hpp"
+#include <memory>
+#include "GLFW/glfw3.h"
+#include "ImGUI/Implementations/imgui_impl_glfw.h"
 
 //==============================================================================================================================================//
-//  Event types.																																	//
+//  Event types.																																//
 //==============================================================================================================================================//
 
 enum EventType
@@ -16,7 +19,7 @@ enum EventType
 	// Mouse events.
 	MousePress			=	1 << 0,
 	MouseRelease		=	1 << 1,
-	MouseButonLeft		=	1 << 2,
+	MouseButtonLeft		=	1 << 2,
 	MouseButtonRight	=	1 << 3,
 	MouseButtonMiddle	=	1 << 4,
 	MouseScroll			=	1 << 5,
@@ -28,7 +31,18 @@ enum EventType
 
 	// Window events.
 	WindowResize		=	1 << 9,
+
+	// Serialisation events.
+	FileDrop			=	1 << 10,
+	FolderDrop			=	1 << 11
+
 };
+
+// Check if an ID contains a specific type.
+bool operator==(uint64_t id, EventType eventType) 
+{
+	return id & eventType;
+}
 
 //==============================================================================================================================================//
 //  Event Class.																																//
@@ -70,6 +84,9 @@ private:
 		m_eventID = eventID; 
 	}
 
+	// Destructor.
+	virtual ~Event() = default;
+
 	// ID describing the event.
 	uint64_t m_eventID;
 
@@ -94,7 +111,7 @@ public:
 	// Returns the position of the mouse event in pixels.
 	glm::vec2& getPositionPixels() 
 	{
-		return m_mousePositionPixels;
+		return m_mousePositionGLFW;
 	}
 
 private:
@@ -108,11 +125,11 @@ private:
 	MouseEvent(glm::vec2& positionPixels, uint64_t eventID) 
 		: Event(eventID) 
 	{
-		m_mousePositionPixels = positionPixels;
+		m_mousePositionGLFW = positionPixels;
 	};
 
 	// The postion of the mouse event.
-	glm::vec2 m_mousePositionPixels = { 0.f, 0.f };
+	glm::vec2 m_mousePositionGLFW = { 0.f, 0.f };
 };
 
 // ------------------------- //
@@ -203,6 +220,8 @@ private:
 //  Window events.																																//
 //==============================================================================================================================================//
 
+// This might rather be handled differently, since ImGUI keeps track
+// of window sizes.
 class WindowResizeEvent : public Event
 {
 
@@ -221,6 +240,172 @@ private:
 	glm::vec2 m_windowSize = { 0.f, 0.f };
 
 };
+
+//==============================================================================================================================================//
+//  Serialisation events.																														//
+//==============================================================================================================================================//
+
+// ------------------- //
+//  F I L E   D R O P  //
+// ------------------- //
+
+class FileDropEvent : public Event
+{
+
+		
+};
+
+// ----------------------- //
+//  F O L D E R   D R O P  //
+// ----------------------- //
+
+class FolderDropEvent : public Event
+{
+
+
+};
+
+
+//==============================================================================================================================================//
+//  Event states.																																//
+//==============================================================================================================================================//
+
+struct EventLog
+{
+	// Mouse events.
+	std::unique_ptr<MouseButtonEvent> mouseButtonEvent = nullptr;
+	std::unique_ptr<MouseMoveEvent>   mouseMoveEvent = nullptr;
+	std::unique_ptr<MouseScrollEvent> mouseScrollEvent = nullptr;
+	
+	// Key events.
+	std::unique_ptr<KeyEvent> keyEvent = nullptr;
+
+	// Window events.
+	std::unique_ptr<WindowResizeEvent> windowResizeEvent = nullptr;
+
+	// Serialisation events.
+	std::unique_ptr<FileDropEvent> fileDropEvent = nullptr;
+	std::unique_ptr<FolderDropEvent> folderDropEvent = nullptr;
+
+	// Add event to the state.
+	void onEvent(Event& event)
+	{
+		// Mouse events.
+		if		(event.getID() == EventType::MouseMove)		{ mouseMoveEvent	= std::make_unique<MouseMoveEvent>(dynamic_cast<MouseMoveEvent&>(event)); }
+		else if (event.getID() == EventType::MouseScroll)	{ mouseScrollEvent	= std::make_unique<MouseScrollEvent>(dynamic_cast<MouseScrollEvent&>(event)); }
+		else if	(event.getID() == EventType::MousePress)	{ mouseButtonEvent	= std::make_unique<MouseButtonEvent>(dynamic_cast<MouseButtonEvent&>(event)); }
+		else if	(event.getID() == EventType::MouseRelease)	{ mouseButtonEvent  = std::make_unique<MouseButtonEvent>(dynamic_cast<MouseButtonEvent&>(event)); }
+
+		// Key events.
+		else if (event.getID() == EventType::KeyPress)		{ keyEvent			= std::make_unique<KeyEvent>(dynamic_cast<KeyEvent&>(event)); }
+		else if (event.getID() == EventType::KeyRelease)	{ keyEvent			= std::make_unique<KeyEvent>(dynamic_cast<KeyEvent&>(event)); }
+
+		// Window events.
+		else if (event.getID() == EventType::WindowResize)	{ windowResizeEvent = std::make_unique<WindowResizeEvent>(dynamic_cast<WindowResizeEvent&>(event)); }
+
+		// Serialisation events.
+		else if (event.getID() == EventType::FileDrop)		{ fileDropEvent		= std::make_unique<FileDropEvent>(dynamic_cast<FileDropEvent&>(event)); }
+		else if (event.getID() == EventType::FolderDrop)	{ folderDropEvent	= std::make_unique<FolderDropEvent>(dynamic_cast<FolderDropEvent&>(event)); }
+	}
+};
+
+// State used throughout the application.
+EventLog eventLog;
+
+//==============================================================================================================================================//
+//  GLFW.																																		//
+//==============================================================================================================================================//
+
+void mouseButtonEvent(GLFWwindow* window, int button, int action, int mods)
+{
+	// Create the event ID.
+	uint64_t eventID = 0;
+
+	// Event type.
+	if		(action == GLFW_PRESS)				 { eventID |= EventType::MousePress; }
+	else if (action == GLFW_RELEASE)			 { eventID |= EventType::MouseRelease; }
+
+	// Button states.
+	if		(button == GLFW_MOUSE_BUTTON_LEFT)   { eventID |= EventType::MouseButtonLeft; }
+	else if (button == GLFW_MOUSE_BUTTON_RIGHT)  { eventID |= EventType::MouseButtonRight; }
+	else if (button == GLFW_MOUSE_BUTTON_MIDDLE) { eventID |= EventType::MouseButtonMiddle; }
+
+	// Get the cursos position.
+	glm::vec2 cursorPos;
+	glfwGetCursorPos(window, (double*) &cursorPos.x, (double*) & cursorPos.y);
+
+	// Log event.
+	MouseButtonEvent event(cursorPos, eventID);
+	eventLog.onEvent(event);
+
+	// ImGui mouse callback.
+	ImGui_ImplGlfw_MouseButtonCallback(window, button, action, mods);
+}
+
+void mouseMoveEvent(GLFWwindow* window, double xpos, double ypos)
+{
+	// Event ID.
+	uint64_t eventID = EventType::MouseMove;
+	
+	// Set button states.
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT))   { eventID |= EventType::MouseButtonLeft; }
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT))  { eventID |= EventType::MouseButtonRight; }
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE)) { eventID |= EventType::MouseButtonMiddle; }
+
+	// Get the cursos position.
+	glm::vec2 cursorPos;
+	glfwGetCursorPos(window, (double*)&cursorPos.x, (double*)&cursorPos.y);
+
+	// Log event.
+	MouseMoveEvent event(cursorPos, eventID);
+	eventLog.onEvent(event);
+}
+
+void mouseScrollEvent(GLFWwindow* window, double xoffset, double yoffset)
+{
+	// Event ID.
+	uint64_t eventID = EventType::MouseScroll;
+
+	// Set button states.
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT))   { eventID |= EventType::MouseButtonLeft; }
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT))  { eventID |= EventType::MouseButtonRight; }
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE)) { eventID |= EventType::MouseButtonMiddle; }
+
+	// Get the cursos position.
+	glm::vec2 cursorPos;
+	glfwGetCursorPos(window, (double*)&cursorPos.x, (double*)&cursorPos.y);
+
+	// Log event.
+	MouseScrollEvent event(cursorPos, yoffset, eventID);
+	eventLog.onEvent(event);
+
+	// ImGui scroll callback.
+	ImGui_ImplGlfw_ScrollCallback(window, xoffset, yoffset);
+}
+
+void keyEvent(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	// Event ID.
+	uint64_t eventID = 0;
+
+	// Event type.
+	if	    (action == GLFW_PRESS)   { eventID |= EventType::KeyPress; }
+	else if (action == GLFW_RELEASE) { eventID |= EventType::KeyRelease; }
+
+	// Log event.
+	KeyEvent event(key, eventID);
+	eventLog.onEvent(event);
+
+	// ImGui key callback.
+	ImGui_ImplGlfw_KeyCallback(window, key, scancode, action, mods);
+}
+
+void fileDropEvent(GLFWwindow* window, int count, const char** paths)
+{
+	/*graphicsHandler->m_fileDropEvent.eventTrigger = true;
+	graphicsHandler->m_fileDropEvent.totalFiles = count;
+	for (int i = 0; i < count; i++) { graphicsHandler->m_fileDropEvent.paths.push_back(paths[i]); }*/
+}
 
 //==============================================================================================================================================//
 //  EOF.																																		//
