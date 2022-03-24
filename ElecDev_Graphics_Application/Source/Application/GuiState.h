5fa5a980ee197f1cdcc63ec50c753a6fea53139a
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-03-24 15:25:05 +0200

    Cleaned up GuiState

--- ElecDev_Graphics_Application/Source/Application/GuiState.h
+++ ElecDev_Graphics_Application/Source/Application/GuiState.h
@@ -33,36 +33,8 @@ struct GUIState
 	// Default constructor.
 	GUIState() {}
 
-	// Window States.
-	bool toolsExpanded = false;
-	bool drawMCC = false;
-	bool blockDiagram = false;
-	bool circuitBucket = false;
-	bool startMainGraphics = true;
-	bool startDemoWindow = false;
-	bool showGraphicsWindow = true;
-	bool circuitEditor = false;
-
 	// Context menu states.
 	Context clickedZone;
-
-	// Popup menu.
-	glm::vec2 popUpPosition = { 0.f, 0.f };
-	bool popUpMenu = false;
-	bool componentEditor = false;
-
-	// Action states.
-	bool renderWindowHovered = false;
-	bool renderResizeEvent = true;
-	bool globalQuit = false;
-	bool userWindowHovered = false;
-
-	// ImGui Variables.
-	glm::vec2 imguiGlobalMouseCoords = { 0.f, 0.f };
-
-	// Render windows information.
-	glm::vec2 renderWindowMouseCoordinate = { 0,0 };
-	glm::vec2 renderWindowSize = { 500,500 };
 
 	// Engine.
 	bool portHovered = false;
@@ -71,7 +43,6 @@ struct GUIState
 	//Design Engine
 	Component2D* active_component = NULL;
 	Cable* active_cable = nullptr;
-
 };
 
 /*=======================================================================================================================================*/
