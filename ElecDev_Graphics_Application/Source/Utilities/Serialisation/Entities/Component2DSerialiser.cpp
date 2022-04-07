9283120493251f070cc4e70498806fe35b9cf077
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-04-07 21:16:26 +0200

    Work on Deserialiser

--- ElecDev_Graphics_Application/Source/Utilities/Serialisation/Entities/Component2DSerialiser.cpp
+++ ElecDev_Graphics_Application/Source/Utilities/Serialisation/Entities/Component2DSerialiser.cpp
@@ -25,21 +25,8 @@ YAML::Emitter& operator<<(YAML::Emitter& emitter, std::shared_ptr<Component2D>& 
 	emitter << YAML::Key << "Internal circuit" << YAML::Value << "Test AE 234";
 	emitter << YAML::Key << "Dictionary" << YAML::Value << comp->dataDict;
 
-	// Shape.
-	emitter << YAML::Key << "Polygon" << YAML::Value;
-	emitter << YAML::BeginMap;
-		emitter << YAML::Key << "Colour" << YAML::Value << comp->shapeColour;
-	emitter << YAML::EndMap;
-
 	// Title.
-	emitter << YAML::Key << "Title" << YAML::Value;
-	emitter << YAML::BeginMap;
-		emitter << YAML::Key << "String" << YAML::Value << comp->titleString;
-		emitter << YAML::Key << "EquipType" << YAML::Value << comp->equipType;
-		emitter << YAML::Key << "Size" << YAML::Value << comp->titleSize;
-		emitter << YAML::Key << "Offset" << YAML::Value << comp->titleOffset;
-		emitter << YAML::Key << "Colour" << YAML::Value << comp->titleColour;
-	emitter << YAML::EndMap;
+	emitter << YAML::Key << "Title" << YAML::Value << comp->title;
 
 	// Ports.
 	emitter << YAML::Key << "Ports" << YAML::Value;
