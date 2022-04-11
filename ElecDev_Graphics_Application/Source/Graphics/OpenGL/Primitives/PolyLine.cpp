a36439e39dba8500e42d2d247419eaa586aa10e3
Cullen Stewart-Burger <cullen.steward-burger@enerdyne.co.za>
2022-04-12 01:02:42 +0200

    Polylines working

--- ElecDev_Graphics_Application/Source/Graphics/OpenGL/Primitives/PolyLine.cpp
+++ ElecDev_Graphics_Application/Source/Graphics/OpenGL/Primitives/PolyLine.cpp
@@ -4,9 +4,9 @@
 #include "OpenGL/Primitives/Vertex.h"
 #include <Clipper/cpp/clipper.hpp>
 
-PolyLine::PolyLine(std::vector<glm::vec2> vertices, VertexArrayObject<VertexData>* VAO, Entity* parent) :Polygon2D({}, VAO, parent)
+PolyLine::PolyLine(std::vector<glm::vec2> vertices, VertexArrayObject<VertexData>* VAO, Entity* parent) :Polygon2D({}, VAO, parent), m_vertices(vertices)
 {
-	/*
+	/*CVAC implementation (not working)
 	std::vector<cavc::PlineVertex<float>> verts;
 	std::transform(vertices.begin(), vertices.end(), std::back_inserter(verts), [&](glm::vec2 in) {
 		return glm::vec3(in*1000.f, 0.f);
@@ -37,44 +37,109 @@ PolyLine::PolyLine(std::vector<glm::vec2> vertices, VertexArrayObject<VertexData
 			});
 		resultVec.push_back(temp);
 	}*/
+	update();
+}
 
-	ClipperLib::Path subj; 
+void PolyLine::update()
+{
+
+	ClipperLib::Path subj;
 	ClipperLib::Paths solution;
-	unsigned sf = 1000;
-	std::transform(vertices.begin(), vertices.end(), std::back_inserter(subj), [&](glm::vec2 in) {
-		return ClipperLib::IntPoint(static_cast<ClipperLib::cInt>(in.x*sf), static_cast<ClipperLib::cInt>(in.y*sf));
+	float sf = 1000;
+	std::transform(m_vertices.begin(), m_vertices.end(), std::back_inserter(subj), [&](glm::vec2 in) {
+		return ClipperLib::IntPoint(static_cast<ClipperLib::cInt>(in.x * sf), static_cast<ClipperLib::cInt>(in.y * sf));
 		});
 	ClipperLib::ClipperOffset co;
 	co.AddPath(subj, ClipperLib::jtSquare, ClipperLib::etOpenSquare);
-	co.Execute(solution, m_thickness/2 * sf);
+	co.Execute(solution, m_thickness / 2 * sf);
 
 	std::vector<std::vector<glm::vec3>> resultVec;
-	for (ClipperLib::Path path : solution) {
+	for (ClipperLib::Path& path : solution) {
 		std::vector<glm::vec3> temp;
 		std::transform(path.begin(), path.end(), std::back_inserter(temp), [&](ClipperLib::IntPoint in) {
-			return glm::vec3(in.X/sf, in.Y/sf , 0.f);
+			return glm::vec3(in.X / sf, in.Y / sf, m_layer);
 			});
 		resultVec.push_back(temp);
 	}
 
 	m_VAO->popPrimitive(this);
 	std::vector<unsigned> indices = mapbox::earcut<unsigned>(resultVec);
-	m_vertexCount = resultVec[0].size();
 	m_indexCount = indices.size();
 
 
-
-
 	std::vector<VertexData> vertexVector;
-	for (auto& vertex : resultVec[0])
-		vertexVector.emplace_back(VertexData(vertex, m_colour, m_entityID));
+	for (auto& path : resultVec) {
+		for (auto& vertex : path)
+			vertexVector.emplace_back(VertexData(vertex, m_colour, m_entityID));
+	}
+
+
+	m_vertexCount = vertexVector.size();
 	m_VAO->pushPrimitive(this, vertexVector, indices);
 }
 
-void PolyLine::addSegment(glm::vec2 nextPoint)
+void PolyLine::pushVertex(const glm::vec3& vertex)
 {
+	m_vertices.push_back({ vertex });
+	update();
 }
 
-void PolyLine::translateVertex(Circle* vertex, const glm::vec2 translation)
+void PolyLine::translateVertexAtIndex(unsigned index, const glm::vec3& translation)
 {
+	translateVertexAtIndex(index, glm::vec2{ translation });
+}
+
+void PolyLine::translateVertexAtIndex(unsigned index, const glm::vec2& translation)
+{
+	m_vertices.at(index) += translation;
+	update();
+}
+
+void PolyLine::translateToVertexAtIndex(unsigned index, const glm::vec3& position)
+{
+	translateToVertexAtIndex(index, glm::vec2{ position });
+}
+
+void PolyLine::translateToVertexAtIndex(unsigned index, const glm::vec2& position)
+{
+	glm::vec2 translation = position - m_vertices.at(index);
+	translateVertexAtIndex(index, translation);
+}
+
+void PolyLine::translateVertexTo(VertexData* vertex, const glm::vec3 position)
+{
+	translateVertexTo(vertex, glm::vec2{ position });
+}
+
+void PolyLine::translateVertexTo(VertexData* vertex, const glm::vec2 position)
+{
+	auto it = std::find_if(begin(m_vertices), end(m_vertices), [&](glm::vec2 vert)
+		{
+			return vert == glm::vec2{ vertex->data.position };
+		});
+	if (it == end(m_vertices)) {
+		LUMEN_LOG_WARN("Tried to move an invlaid vertex.", "PolyLine");
+		return;
+	}
+	*it = position;
+	update();
+}
+
+void PolyLine::translateVertex(VertexData* vertex, const glm::vec3 translation)
+{
+	translateVertex(vertex, glm::vec2{ translation });
+}
+
+void PolyLine::translateVertex(VertexData* vertex, const glm::vec2 translation)
+{
+	auto it = std::find_if(begin(m_vertices), end(m_vertices), [&](glm::vec2 vert)
+		{
+			return vert == glm::vec2{ vertex->data.position };
+		});
+	if (it == end(m_vertices)) {
+		LUMEN_LOG_WARN("Tried to move an invlaid vertex.", "PolyLine");
+		return;
+	}
+	*it += translation;
+	update();
 }
--- ElecDev_Graphics_Application/Source/Graphics/OpenGL/Primitives/PolyLine.h
+++ ElecDev_Graphics_Application/Source/Graphics/OpenGL/Primitives/PolyLine.h
@@ -11,12 +11,28 @@ class PolyLine :
 
     public:
     float m_thickness = 0.005f;
+    //we need to retain a copy of the raw vertices for editing
+    std::vector<glm::vec2> m_vertices;
+	float m_layer = 0.f;
     
     PolyLine(std::vector<glm::vec2> vertices, VertexArrayObject<VertexData>* VAO, Entity* parent);
-    //Add a new line segment
-    void addSegment(glm::vec2 nextPoint);
-    // Move a vertex
-    void translateVertex(Circle* vertex, const glm::vec2 translation);
+    //Calculates the offset and runs the triangulation function
+    void update();
+	virtual void pushVertex(const glm::vec3& vertex) override;
+	virtual void translateVertexAtIndex(unsigned index, const glm::vec3& translation) override;
+	virtual void translateVertexAtIndex(unsigned index, const glm::vec2& translation) override;
+	// Translate to.
+	virtual void translateToVertexAtIndex(unsigned index, const glm::vec3& position) override;
+	virtual void translateToVertexAtIndex(unsigned index, const glm::vec2& position) override;
+	// Move a vertex
+	virtual void translateVertexTo(VertexData* vertex, const glm::vec3 position) override;
+	// Move a vertex
+	virtual void translateVertexTo(VertexData* vertex, const glm::vec2 position) override;
+	// Move a vertex
+	virtual void translateVertex(VertexData* vertex, const glm::vec3 translation) override;
+	// Move a vertex
+	virtual void translateVertex(VertexData* vertex, const glm::vec2 translation) override;
+
 
 
 };
