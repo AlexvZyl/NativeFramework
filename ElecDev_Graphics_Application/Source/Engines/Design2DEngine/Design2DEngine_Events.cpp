d13a5ba86cf7621aaf12e75c404a693b257a5c31
Cullen Stewart-Burger <cullen.steward-burger@enerdyne.co.za>
2022-04-13 09:16:33 +0200

    Improved Cable dragging

--- ElecDev_Graphics_Application/Source/Engines/Design2DEngine/Design2DEngine_Events.cpp
+++ ElecDev_Graphics_Application/Source/Engines/Design2DEngine/Design2DEngine_Events.cpp
@@ -45,14 +45,24 @@ void Design2DEngine::onMouseButtonEvent(MouseButtonEvent& event)
 			glm::vec2 coords = { pixelCoords[0], pixelCoords[1] };
 			m_currentEntityID = getEntityID(coords);
 
-			setActiveComponent(m_currentEntityID);
-			setActiveCable(m_currentEntityID);
-			Port* clickedPort = getPort(m_currentEntityID);
-			if (clickedPort != nullptr)
-			{
-				m_activeCable = nullptr;
-				designerState = CABLE_PLACE;
-				m_activeCable = std::make_shared<Cable>(clickedPort, m_circuit.get());
+			if (m_activeCable.get()) {
+				auto [vertexPtr, distance] = m_activeCable->getNearestVertex(screenCoords);
+				m_activeVertex = nullptr;
+				if (distance < clickTol)
+				{
+					m_activeVertex = vertexPtr;
+				}
+			}
+			if (!m_activeVertex) {
+				setActiveComponent(m_currentEntityID);
+				setActiveCable(m_currentEntityID);
+				Port* clickedPort = getPort(m_currentEntityID);
+				if (clickedPort != nullptr)
+				{
+					m_activeCable = nullptr;
+					designerState = CABLE_PLACE;
+					m_activeCable = std::make_shared<Cable>(clickedPort, m_circuit.get());
+				}
 			}
 		}
 		else if (designerState == CABLE_PLACE)
@@ -187,17 +197,9 @@ void Design2DEngine::onMouseDragEvent(MouseDragEvent& event)
 		
 			if (m_activeComponent.get()) {
 				m_activeComponent->move(translation);
-			}
-			if (m_activeCable.get()) {
-				auto [vertexPtr, distance] = m_activeCable->getNearestVertex(screenCoords);
-				VertexData* activeVertex = nullptr;
-				if (distance < clickTol)
-				{
-					activeVertex = vertexPtr;
-				}
-				if (activeVertex) {
-					m_activeCable->translateVertex(activeVertex, translation);
-				}
+			}	
+			if (m_activeVertex) {
+				m_activeCable->translateVertex(m_activeVertex, translation);
 			}
 	}
 }
