c3f613d2193dcb814255d43bad0e0bebd8c773ab
Alexander van Zyl <alexandervanzyl@protonmail.com>
2022-06-06 19:05:53 +0200

    Fix focus event

--- ElecDev_Graphics_Application/Source/Application/ApplicationGLFWWindow.cpp
+++ ElecDev_Graphics_Application/Source/Application/ApplicationGLFWWindow.cpp
@@ -392,7 +392,7 @@ GLFWwindow* Application::glfwInitWindow()
     // --------------------- //
 
     // Enable MSAA.
-    glfwWindowHint(GLFW_SAMPLES, 8);
+    glfwWindowHint(GLFW_SAMPLES, 16);
 
     // Create GLFW window.
     GLFWwindow* window = glfwCreateWindow(1280, 720, "Lumen", NULL, NULL);
