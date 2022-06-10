28aec07a54b701e4bbfe150cdce86a9f90b7fb0e
Cullen Stewart-Burger <cullen.steward-burger@enerdyne.co.za>
2022-06-10 16:23:22 +0200

    Started transformer setup GUI
    Incomplete transformer setup gui

---
+++ ElecDev_Graphics_Application/Source/GUI/TransformerSetup/TransformerSetupGui.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Application/LumenWindow/LumenWebsocketWindow.h"
+class TransformerSetupGui :
+    public LumenWebsocketWindow
+{
+private:
+    std::unordered_map<std::string, std::string[2]> m_dataDict;
+    const char* const transformerSizes[2] = {"2500", "2000"};
+    int transformerSizeIdx = 0;
+    int utilizationTarget = -1;
+
+public:
+    TransformerSetupGui(const std::string& name, std::unordered_map<std::string, std::string[2]>& dataDict, int imguiWindowFlags = 0);
+
+    virtual void onImGuiBegin() override;
+    virtual void onImGuiRender() override;
+    virtual void onImGuiEnd() override;
+
+};
+
